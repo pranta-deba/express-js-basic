9845e9c9ed8eeb9a55a78a4e599bc954ec4b7f08
Pranto Deb <rajprit0m@outlook.com>
2025-01-21 15:32:35 +0600

    parsers, request and response object

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// parsers
+app.use(express_1.default.json());
+app.use(express_1.default.text());
 app.get("/", (req, res) => {
     res.send("Hello world!");
+});
+app.post("/post", (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: "post successfully",
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,19 @@
 import express from "express";
 const app = express();
 
+// parsers
+app.use(express.json());
+app.use(express.text());
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
+});
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.json({
+    message: "post successfully",
+  });
 });
 
 export default app;
